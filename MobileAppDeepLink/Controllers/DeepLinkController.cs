5540d0b1691042776c73693e529b8e99150097d5
Thap Luong <quangthap9x@gmail.com>
2021-08-07 16:18:31 +0700

    first commit

---
+++ MobileAppDeepLink/Controllers/DeepLinkController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MobileAppDeepLink.Controllers
+{
+    [Route(".well-known")]
+    public class DeepLinkController : Controller
+    {
+        private IHostingEnvironment _hostingEnvironment;
+
+        public DeepLinkController(IHostingEnvironment hostingEnvironment)
+        {
+            _hostingEnvironment = hostingEnvironment;
+        }
+
+        [Route("apple-app-site-association")]
+        public async Task<IActionResult> Apple()
+        {
+            return Content(await System.IO.File.ReadAllTextAsync(Path.Combine(_hostingEnvironment.WebRootPath, "apple-app-site-association")),
+                "text/plain");
+        }
+
+        [Route("assetlinks.json")]
+        public async Task<IActionResult> Android()
+        {
+            return Content(await System.IO.File.ReadAllTextAsync(Path.Combine(_hostingEnvironment.WebRootPath, "assetlinks.json")),
+                "application/json");
+        }
+    }
+}
---
+++ MobileAppDeepLink/Controllers/MobileRedirectionController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MobileAppDeepLink.Controllers
+{
+    [Route("mobile-redirection")]
+    public class MobileRedirectionController : Controller
+    {
+        [Route("detail-video/{id}")]
+        [HttpGet]
+        public async Task<string> DetailVideo([FromRoute] int id)
+        {
+            return "Không có gì đâu mà vào!!";
+        }
+
+    }
+}
